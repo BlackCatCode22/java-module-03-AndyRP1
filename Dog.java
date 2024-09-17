a711c9d7ecc466e5560dc22c1859592ae496377c
AndyRP1 <0960247@my.scccd.edu>
2024-09-17 12:12:43 -0700

    Add files via upload

--- Dog.java
+++ Dog.java
@@ -1,4 +1,54 @@
-package myanimals;
-
 public class Dog {
+//Create two fields for our dog objects.
+
+    String name;
+    no usages
+    int age;
+
+    // Create a contstructor for the dog objects that will be created with the new keyword.
+    // A constroctor is a special method that is called or invoked when... The Dog class is used
+    // to create an object.
+    public Dog()
+        System.out.println("\n A dog obeject was created. This message is from the constructor\n");
+}
+
+//Create a constructor that accepts a string data type for the dogs name .
+public Dog(String myName) {
+    this.name = myName;
+}
+
+
+// Create a contructor that accepts an argument when an object is created, this argument will
+//be an int
+public Dog(int theAge)
+    this.age = theAge;
+Public Dog(int theAge)
+      this.age = theAge;
+    }
+// Create a controctor that accepts two arguments - a string for the name , and an int or age
+public Dog String()
+        this.name = myName;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
---
+++ MainClassPractice.java
@@ -0,0 +1,39 @@
+//ar 9/17/24
+
+
+
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MainClassPractice {
+    public static <Dog> void main(String[] args) {
+
+        System.out.printf("Welcome to my OOP Class Practice Program\n");
+
+
+        // Create a couple of Dog objects
+        // with an empty
+        Dog myDog = new Dog ();
+        Dog anotherDog = new Dog()
+
+
+        // Create a dog with a string name
+        Dog aDog = new Dog("Spot")
+
+        //output the new dog objects name.
+        System.out.println("\nThe new dog's name is:" + aDog.Name);
+
+        // Create a fourth dog with an int age.
+        Dog fourthDog = new Dog(theAge:33)
+
+        //Output the dog's age.
+        System.out.println(x"\ my new dog's age is " + fourthDog);
+
+        // Snf.... finally create a fith and last dog...
+
+        Dog fithDog = new Dog(someName:" my new dog's name," someAge 7)
+
+        // Validate it.
+        System.out.println("my last dog's name is "+ fithDog.name + "and age is "+ fithDog.age);
+
+    }
+}
+}
